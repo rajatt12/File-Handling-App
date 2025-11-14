abb25f382111985d00f3b505e35206e16188b925
Rajatveer Singh Pasricha <rajatveer1234@gmail.com>
2025-11-14 11:40:58 +0530

    Add files via upload

---
+++ FileHandling.py
@@ -0,0 +1,7 @@
+p=open(r"C:\Users\RPC\Downloads\Rajat.txt",encoding="utf-8")
+print(p.read())
+
+
+l=open("Veer.txt",'a') 
+l.write("  Rajatveer Singh")#we ran this program 2 times so it printed Rajatveer Singh 2 times
+l.close()
